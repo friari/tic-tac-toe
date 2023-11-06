384ff9c4bf636bb21f38611bd222b998f71ee514
Ari <afriedgut1@gmail.com>
2023-11-06 15:46:25 +1100

    ability to get and set player name, and get player id. modals styled.

--- script.js
+++ script.js
@@ -12,98 +12,177 @@ const gameboard = (() => {
 })();
 
 // Player, sets player token
-const createPlayer = (name, token) => {
+const createPlayer = (name, token, num) => {
   let _playerWins = 0;
+  const _playerId = `player${num}`;
+  let _playerName = name;
+
+  const getId = () => _playerId;
+
+  const getName = () => _playerName;
+  const setName = (name) => _playerName = name;
+
   const increaseWins = () => _playerWins++;
   const printWins = () => _playerWins;
 
-  return { token, increaseWins }
+  return { token, increaseWins, printWins, getId, getName, setName }
 }
 
-const runGame = (board) => {
-  let hasWin = false;
-  const player1 = createPlayer("X");
-  const player2 = createPlayer("O");
-
+const controller = (() => {
   const gameboardDom = document.querySelector('[data-board]');
   const gameCellsDom = document.querySelectorAll('[data-cell]');
+  const modalContainer = document.querySelector('[data-modal-container]');
+  const playButtons = document.querySelectorAll('[data-start-game]');
+  const changeNameTriggers = document.querySelectorAll('[data-change-name-trigger]');
+  const changeNameButtons = document.querySelectorAll('[data-submit-name]')
 
-  let _currentPlayer = player1;
+  const player1 = createPlayer("Player One", "X", 1);
+  const player2 = createPlayer("Player Two", "O", 2);
 
-  const _gameRound = (e) => {
-    const target = e.target;
-    const currentCell = target.getAttribute('data-cell');
-    
-    // returning if spot is taken
-    if (board[currentCell]) return;
+  const players = [player1, player2];
 
-    board[currentCell] = _currentPlayer.token;
-    target.classList.add(`is-${_currentPlayer.token}`);
+  modalContainer.addEventListener('click', (e) => {
+    if (e.target === modalContainer) {
+      _closeModal();
+    }
+  });
 
-    hasWin = _checkForWin();
-    
-    if (hasWin) {
-      _currentPlayer._playerWins++;
-      console.log(`congratulations ${_currentPlayer.token}, you have won the game!`);
-      setTimeout(() => {
-        gameCellsDom.forEach(elem => {
-          elem.classList.remove('is-X', 'is-O');
-        });
-        board = gameboard.init();
-        hasWin = false;
-      }, 2000);
+  changeNameTriggers.forEach(button => {
+    button.addEventListener('click', (e) => {
+      const playerId = e.target.closest('[data-player]').getAttribute('data-player');
+      const playerDetails = players.find(player => playerId === player.getId());
+      _openModal("name", playerDetails);
+    })
+  });
+
+  changeNameButtons.forEach(button => {
+    button.addEventListener('click', (e) => {
+      const parentContainer = e.target.closest('[data-player-name]');
+      const playerId = parentContainer.getAttribute('data-player-name');
+      const playerCard = document.querySelector(`[data-player=${playerId}`);
+      console.log(playerCard);
+      const playerToChange = players.find(player => playerId === player.getId());
+      const input = parentContainer.querySelector('.input');
+
+      playerToChange.setName(input.value);
+      playerCard.querySelector('[data-player-name]').innerText = playerToChange.getName();
+
+      input.value = '';
+
+      _closeModal();
+    })
+  });
+
+  const _cleanBoard = () => {
+    gameCellsDom.forEach(elem => {
+      elem.classList.remove('is-X', 'is-O');
+    });
+  }
+
+  const _closeModal = () => {
+    modalContainer.classList.add('hidden');
+  }
+
+  const _openModal = (modalType, playerDetails) => {
+    modalContainer.classList.remove('hidden');
+
+    if (modalType === "win") {
+      modalContainer.classList.add('is-win');
+      modalContainer.querySelector('[data-winning-player]').innerText = playerDetails.getName();
       return;
     }
 
-    _currentPlayer = _currentPlayer === player1 ? player2 : player1;
+    modalContainer.classList.add('is-player');
+    modalContainer.querySelector('.modal__player').setAttribute('data-player-name', playerDetails.getId());
   }
 
-  const _checkForWin = () => {
-    const checkWinner = (currentLine) => {
-      return currentLine.every(val => val != undefined && val === currentLine[0]);
-    };
-    
-    //checking rows
-    for (i = 0; i <= 6; i += 3) {
-      let currentLine = [];
-      for (j = 0; j < 3; j++) {
-        let currentIndex = i + j;
-        currentLine.push(board[currentIndex]);
+  playButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      if (!modalContainer.classList.contains('hidden')) {
+        _closeModal();
       }
-      if (checkWinner(currentLine)) return currentLine[0];
-    }
 
-    //checking columns
-    for (i = 0; i < 3; i++) {
-      let currentLine = [];
-      for (j = 0; j <= 6; j += 3) {
-        let currentIndex = i + j;
-        currentLine.push(board[currentIndex]);
+      runGame(gameboard.init());
+    });
+  })
+
+  const runGame = (board) => {
+    _cleanBoard();
+    let hasWin = false;
+  
+    let _currentPlayer = player1;
+  
+    const _gameRound = (e) => {
+      const target = e.target;
+      const currentCell = target.getAttribute('data-cell');
+      
+      // returning if spot is taken
+      if (board[currentCell]) return;
+  
+      board[currentCell] = _currentPlayer.token;
+      target.classList.add(`is-${_currentPlayer.token}`);
+  
+      hasWin = _checkForWin();
+      
+      if (hasWin) {
+        _currentPlayer.increaseWins();
+        console.log(`congratulations ${_currentPlayer.token}, you have won the game!`);
+        gameboardDom.removeEventListener('click', _gameRound);
+        setTimeout(() => {
+          board = gameboard.init();
+          hasWin = false;
+        }, 2000);
+        return;
       }
-      if (checkWinner(currentLine)) return currentLine[0];
+  
+      _currentPlayer = _currentPlayer === player1 ? player2 : player1;
     }
-
-    //checking diagonals
-    for (i = 0; i <= 6; i+=6) {
-      if (i === 0) {
+  
+    const _checkForWin = () => {
+      const checkWinner = (currentLine) => {
+        return currentLine.every(val => val != undefined && val === currentLine[0]);
+      };
+      
+      //checking rows
+      for (i = 0; i <= 6; i += 3) {
         let currentLine = [];
-        for (let j = 0; j <= 8; j += 4) {
+        for (j = 0; j < 3; j++) {
           let currentIndex = i + j;
           currentLine.push(board[currentIndex]);
         }
         if (checkWinner(currentLine)) return currentLine[0];
-      } else {
+      }
+  
+      //checking columns
+      for (i = 0; i < 3; i++) {
         let currentLine = [];
-        for (let j = 0; j <= 4; j += 2) {
-          let currentIndex = i - j;
+        for (j = 0; j <= 6; j += 3) {
+          let currentIndex = i + j;
           currentLine.push(board[currentIndex]);
         }
         if (checkWinner(currentLine)) return currentLine[0];
       }
+  
+      //checking diagonals
+      for (i = 0; i <= 6; i+=6) {
+        if (i === 0) {
+          let currentLine = [];
+          for (let j = 0; j <= 8; j += 4) {
+            let currentIndex = i + j;
+            currentLine.push(board[currentIndex]);
+          }
+          if (checkWinner(currentLine)) return currentLine[0];
+        } else {
+          let currentLine = [];
+          for (let j = 0; j <= 4; j += 2) {
+            let currentIndex = i - j;
+            currentLine.push(board[currentIndex]);
+          }
+          if (checkWinner(currentLine)) return currentLine[0];
+        }
+      }
     }
-  }
-
-  gameboardDom.addEventListener('click', _gameRound);
-};
-
-runGame(gameboard.init());
+  
+    gameboardDom.addEventListener('click', _gameRound);
+  };
+})();
